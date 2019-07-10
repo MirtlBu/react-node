5b6b785ae1224db0d1e9bc71e365e93493bad2c7
mirtlbu <bubuzoglo@gmail.com>
2019-07-10 12:18:33 +0300

    const to var

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const https = require("https");
-const urlOpenWeatherMap = "https://api.openweathermap.org/data/2.5/weather?q=";
-const userKey = "&APPID=caf1764f2fe5e1a34e01377fc1b5e6f0";
+var https = require("https");
+var urlOpenWeatherMap = "https://api.openweathermap.org/data/2.5/weather?q=";
+var userKey = "&APPID=caf1764f2fe5e1a34e01377fc1b5e6f0";
 var express = require('express');
 var app = express();
-var router = express.Router();
 var path = require('path');
-const request = require('request');
+var request = require('request');
 var port = 8000;
 
-const cities = ['Toronto', 'Kuala Lumpur', 'Prague', 'Paris', 'Tokyo', 'NY', 'Barselona', 'Dubai'];
+var cities = ['Toronto', 'Kuala Lumpur', 'Prague', 'Paris', 'Tokyo', 'NY', 'Barselona', 'Dubai'];
 
 app.use(express.static('public'));
 
@@ -25,9 +24,6 @@ app.get('/city', function(req, res) {
     });
     
 });
-
-
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
