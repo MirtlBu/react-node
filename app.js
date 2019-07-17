f1be34ef9dd900ec363923929a3372c556eed7fb
mirtlbu <bubuzoglo@gmail.com>
2019-07-17 12:31:28 +0300

    start from scratch in index.html, get user city from ip and capitals from json

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 var https = require("https");
 var urlOpenWeatherMap = "https://api.openweathermap.org/data/2.5/weather?q=";
-var userKey = "&APPID=caf1764f2fe5e1a34e01377fc1b5e6f0";
+var userKey = "&units=metric&APPID=caf1764f2fe5e1a34e01377fc1b5e6f0";
 var express = require('express');
 var app = express();
 var path = require('path');
 var request = require('request');
 var port = 8000;
+var fs = require('fs');
 
-var cities = ['Toronto', 'Kuala Lumpur', 'Prague', 'Paris', 'Tokyo', 'NY', 'Barselona', 'Dubai'];
+var rawjson = fs.readFileSync('public/src/js/capitals.json');  
+var capitals = JSON.parse(rawjson);
+
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+    
 });
 
-app.get('/city', function(req, res) {
-    var data;
-    //var url = urlOpenWeatherMap + req.url.slice(5) + userKey;
-    var url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=caf1764f2fe5e1a34e01377fc1b5e6f0";
+app.get('/current', function(req, res) {
+
+    function getTemp(data) {
+        var url = urlOpenWeatherMap + data + userKey;
+        request(url, function (err, response, body) {
+            res.send(JSON.parse(body));
+        });
+    }
+
+    request('https://ipinfo.io', function (err, response, body) {
+        var obj = JSON.parse(body);
+        getTemp(obj.city);
+    });
+});
+
+
+app.get('/city/:city', function(req, res) {
+    var url = urlOpenWeatherMap + req.params.city + userKey;
     request(url, function (err, response, body) {
         res.send(JSON.parse(body));
     });
@@ -26,4 +44,3 @@ app.get('/city', function(req, res) {
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
